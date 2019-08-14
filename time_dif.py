432ffce5aa2aafc94f6a1f16b9f13f9ee5113c93
Fred Watts <44913109+stewartm888@users.noreply.github.com>
2019-08-14 15:45:24 -0400

    Add files via upload

--- time_dif.py
+++ time_dif.py
@@ -17,6 +17,9 @@ prior_value = 0
 later_value = 0
 nano_freq = 0
 freq_list = []
+list_var = 0
+list_avg = 0
+
 
 #directory of the files
 dest_dir = "/home/fred/Desktop/g"
@@ -39,28 +42,82 @@ for file_name in glob.glob(os.path.join(dest_dir, '*.csv')):
 
 
 #Print Basic File Info
-	print('\n\nFile',iteration,'/',file_count,': ',file_name)
+	print('\n\nFile',iteration,'/',file_count,': ',file_name[21:])
 	print('Column Count:', columns,' Row Count:', rows) 
-	print('First Timestamp:',prior_value,'\n')
+#	print('First Timestamp:',prior_value)
 
 
 #Iterate rows
-	while DF_End_Flag == 0:
-		try:
-			prior_value = df.iloc[row_cursor,column_cursor]
-			row_cursor = row_cursor + 1
-			later_value = df.iloc[row_cursor,column_cursor]
-			nano_freq = later_value - prior_value
-			print('File:',iteration,'Row',row_cursor,'/',rows,' -- jump (ns):',nano_freq)
-			freq_list.insert(0 , nano_freq)
-		except IndexError:
-			row_cursor = 0
-			prior_value = 0
-			later_value = 0
-			nano_freq = 0
-			DF_End_Flag = 1
+	while row_cursor <= 5000:
+		prior_value = df.iloc[row_cursor,column_cursor]
+		row_cursor = row_cursor + 1
+		later_value = df.iloc[row_cursor,column_cursor]
+		nano_freq = later_value - prior_value
+		freq_list.insert(0 , nano_freq)
+		
+	list_var = np.var(freq_list)
+	list_avg = np.average(freq_list)
+	print('Timestamp (1) Variance:', list_var, '  Avg:',list_avg)
+	
+	row_cursor = 20000
+	freq_list = []
+	while row_cursor <= 25000:
+		prior_value = df.iloc[row_cursor,column_cursor]
+		row_cursor = row_cursor + 1
+		later_value = df.iloc[row_cursor,column_cursor]
+		nano_freq = later_value - prior_value
+		freq_list.insert(0 , nano_freq)
+		
+	list_var = np.var(freq_list)
+	list_avg = np.average(freq_list)
+	print('Timestamp (2) Variance:', list_var, '  Avg:',list_avg)
 
-	DF_End_Flag = 0
+	row_cursor = 50000
+	freq_list = []
+	while row_cursor <= 55000:
+		prior_value = df.iloc[row_cursor,column_cursor]
+		row_cursor = row_cursor + 1
+		later_value = df.iloc[row_cursor,column_cursor]
+		nano_freq = later_value - prior_value
+		freq_list.insert(0 , nano_freq)
+		
+	list_var = np.var(freq_list)
+	list_avg = np.average(freq_list)
+	print('Timestamp (3) Variance:', list_var, '  Avg:',list_avg)
+
+	row_cursor = 80000
+	freq_list = []
+	while row_cursor <= 85000:
+		prior_value = df.iloc[row_cursor,column_cursor]
+		row_cursor = row_cursor + 1
+		later_value = df.iloc[row_cursor,column_cursor]
+		nano_freq = later_value - prior_value
+		freq_list.insert(0 , nano_freq)
+		
+	list_var = np.var(freq_list)
+	list_avg = np.average(freq_list)
+	print('Timestamp (4) Variance:', list_var, '  Avg:',list_avg)
+
+
+	row_cursor = 0
+	freq_list = []
+	while row_cursor <= (rows-2):
+		prior_value = df.iloc[row_cursor,column_cursor]
+		row_cursor = row_cursor + 1
+		later_value = df.iloc[row_cursor,column_cursor]
+		nano_freq = later_value - prior_value
+		freq_list.insert(0 , nano_freq)
+		
+	list_var = np.var(freq_list)
+	list_avg = np.average(freq_list)
+	print('Timestam(Tot) Variance:', list_var, '  Avg:',list_avg)
+
+
+	row_cursor = 0
+	freq_list = []
+
+
+
 
 
 	#except IndexError:
